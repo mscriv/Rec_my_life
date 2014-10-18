f688975ac99401b78d71b048fb7023a0f34b55b1
Michael Scriven <bigscriv03@gmail.com>
2014-10-18 16:09:14 -0400

    Added Bootstap and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :categories do
     resources :events
 end
@@ -8,7 +10,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
